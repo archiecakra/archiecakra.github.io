77efaab83bf2f09440d196f9fb2a9959b37ed273
archiecakra <rchiecakra1@gmail.com>
2023-06-27 06:29:32 +0700

    full using cdn, setcookies

--- js/main.js
+++ js/main.js
@@ -193,25 +193,25 @@
 		$(".fh5co-loader").fadeOut("slow");
 	};
 
-	var counter = function() {
-		$('.js-counter').countTo({
-			 formatter: function (value, options) {
-	      return value.toFixed(options.decimals);
-	    },
-		});
-	};
+	// var counter = function() {
+	// 	$('.js-counter').countTo({
+	// 		 formatter: function (value, options) {
+	//       return value.toFixed(options.decimals);
+	//     },
+	// 	});
+	// };
 
-	var counterWayPoint = function() {
-		if ($('#fh5co-counter').length > 0 ) {
-			$('#fh5co-counter').waypoint( function( direction ) {
+	// var counterWayPoint = function() {
+	// 	if ($('#fh5co-counter').length > 0 ) {
+	// 		$('#fh5co-counter').waypoint( function( direction ) {
 										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( counter , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
-	};
+	// 			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+	// 				setTimeout( counter , 400);					
+	// 				$(this.element).addClass('animated');
+	// 			}
+	// 		} , { offset: '90%' } );
+	// 	}
+	// };
 
 	// Parallax
 	var parallax = function() {
@@ -316,8 +316,8 @@
 		testimonialCarousel();
 		goToTop();
 		loaderPage();
-		counter();
-		counterWayPoint();
+		// counter();
+		// counterWayPoint();
 		startUp();
 	});
 
